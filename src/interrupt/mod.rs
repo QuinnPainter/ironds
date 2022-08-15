048a4b9a97048770ae83d740f9ff073ed0b2e0f5
Quinn Painter <quinn@geekymonkey.com>
2022-08-15 18:51:39 +0100

    Change interrupt macros to use decl_macros

--- src/interrupt/mod.rs
+++ src/interrupt/mod.rs
@@ -2,39 +2,26 @@
 // https://github.com/embassy-rs/atomic-polyfill
 // or could provide a custom impl for the critical-section crate:
 // https://github.com/embassy-rs/critical-section
+use core::ptr;
+use crate::addr;
 
 // these are macros and not functions, so that they will be inlined for both ARM and THUMB
-macro_rules! enable_interrupts_master {
-    () => {{
-        use core::ptr;
-        unsafe { ptr::write_volatile($crate::addr::IME as *mut u32, 1); }
-    }}
+pub macro enable_interrupts_master() {
+    unsafe { ptr::write_volatile(addr::IME as *mut u32, 1); }
 }
-pub(crate) use enable_interrupts_master;
 
-macro_rules! disable_interrupts_master {
-    () => {{
-        use core::ptr;
-        unsafe { ptr::write_volatile($crate::addr::IME as *mut u32, 0); }
-    }}
+pub macro disable_interrupts_master() {
+    unsafe { ptr::write_volatile(addr::IME as *mut u32, 0); }
 }
-pub(crate) use disable_interrupts_master;
 
-macro_rules! is_ime_enabled {
-    () => {{
-        use core::ptr;
-        unsafe { ((ptr::read_volatile($crate::addr::IME as *mut u32) & 1) == 1) }
-    }}
+pub macro is_ime_enabled() {
+    unsafe { ((ptr::read_volatile(addr::IME as *mut u32) & 1) == 1) }
 }
-pub(crate) use is_ime_enabled;
 
-macro_rules! critical_section {
-    ($code:block) => {{
-        let e = $crate::interrupt::is_ime_enabled!();
-        $crate::interrupt::disable_interrupts_master!();
-        { $code }
-        // only re-enable interrupts if they were enabled before this
-        if e { $crate::interrupt::enable_interrupts_master!(); }
-    }}
+pub macro critical_section($code:block) {
+    let e = crate::interrupt::is_ime_enabled!();
+    crate::interrupt::disable_interrupts_master!();
+    { $code }
+    // only re-enable interrupts if they were enabled before this
+    if e { crate::interrupt::enable_interrupts_master!(); }
 }
-pub(crate) use critical_section;
