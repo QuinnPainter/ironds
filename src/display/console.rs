01e53ac921e98f66bc884e67eb43639f4997d78b
Quinn Painter <quinn@geekymonkey.com>
2023-02-21 11:59:32 +0000

    Use NdsCell for CursorPos
    this should make it safe to use the print function in interrupts

--- src/display/console.rs
+++ src/display/console.rs
@@ -2,15 +2,22 @@
 //! 
 //! This module is very limited, and should only really be used for debug purposes.
 
+use bitfield_struct::bitfield;
 use crate::{display, mmio};
+use crate::sync::{NdsCell, NdsCellSafe};
 
 static DEFAULT_FONT: &[u8; 4096] = include_bytes!("../../gfx/font.img.bin");
 const DEFAULT_PALETTE: [u16; 2] = [display::rgb15(0x000000), display::rgb15(0xFFFFFF)];
 const TILES_PER_LINE: usize = 32;
 const BYTES_PER_LINE: usize = TILES_PER_LINE * 2;
 
-static mut CURSOR_X: u8 = 0;
-static mut CURSOR_Y: u8 = 0;
+#[bitfield(u16)]
+struct CursorPos {
+    x: u8,
+    y: u8
+}
+unsafe impl NdsCellSafe for CursorPos {}
+static CURSOR_POS: NdsCell<CursorPos> = NdsCell::new(CursorPos::new());
 
 /// Initialises the text console.
 /// 
@@ -71,26 +78,32 @@ pub fn init_default() {
 /// Make sure you have initialised the console before running this function.
 pub fn print(txt: &str) {
     for mut b in txt.bytes() {
+        // update position every iteration so that if a print happens in main thread
+        // and interrupt at the same time, things won't break
+        let pos = CURSOR_POS.read();
+        let (mut cursor_x, mut cursor_y) = (pos.x(), pos.y());
+
+        // if print goes off the bottom, all further prints should be ignored
+        if cursor_y >= 32 { return; }
+
         if b == b'\n' {
-            unsafe { CURSOR_Y += 1; CURSOR_X = 0; }
-            continue;
-        }
-        // Control chars / extended UTF8 chars get mapped to 0 ("tofu" character)
-        if b < 32 || b > 126 {
-            b = 0;
-        }
-        unsafe { core::ptr::write((0x6200000 + 8192
-            + (CURSOR_Y as usize * BYTES_PER_LINE) 
-            + (CURSOR_X as usize * 2)) as *mut u16, b as u16); }
-        unsafe {
-            CURSOR_X += 1;
-            if CURSOR_X >= 32 {
-                CURSOR_X = 0;
-                CURSOR_Y += 1;
-                // if print goes off the bottom, all further prints should be ignored
-                if CURSOR_Y >= 32 { return; }
+            cursor_x = 0;
+            cursor_y += 1;
+        } else {
+            // Control chars / extended UTF8 chars get mapped to 0 ("tofu" character)
+            if b < 32 || b > 126 {
+                b = 0;
+            }
+            unsafe { core::ptr::write((0x6200000 + 8192
+                + (cursor_y as usize * BYTES_PER_LINE) 
+                + (cursor_x as usize * 2)) as *mut u16, b as u16); }
+            cursor_x += 1;
+            if cursor_x >= 32 {
+                cursor_x = 0;
+                cursor_y += 1;
             }
         }
+        CURSOR_POS.write(CursorPos::new().with_x(cursor_x).with_y(cursor_y));
     }
 }
 
@@ -98,11 +111,12 @@ pub fn print(txt: &str) {
 #[must_use]
 #[inline(always)]
 pub fn get_cursor_pos() -> (u8, u8) {
-    unsafe { (CURSOR_X, CURSOR_Y) }
+    let pos = CURSOR_POS.read();
+    (pos.x(), pos.y())
 }
 
 /// Sets the current position of the text cursor. (0, 0) is the top left.
 #[inline(always)]
 pub fn set_cursor_pos(x: u8, y: u8) {
-    unsafe { CURSOR_X = x & 31; CURSOR_Y = y & 31; }
+    CURSOR_POS.write(CursorPos::new().with_x(x & 31).with_y(y & 31));
 }
